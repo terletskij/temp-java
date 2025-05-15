5f71323ae6a02cd5ef650ac2a4f3f777a4946f00
Bogdan <wwwterleckiy@gmail.com>
2025-05-15 23:46:36 +0300

    test commit: remove previous lab

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car("Toyota", "Camry", 2004, 10000);
-        Car car2 = new Car("Tesla", "Model 3", 2020, 30000);
-
-        car1.setPrice(11000);
-        car2.setPrice(33333);
-
-        System.out.println("Car 1 price: $" + car1.getPrice());
-        System.out.println("Car 2 price: $" + car2.getPrice());
+	System.out.println("test1");
     }
 }
